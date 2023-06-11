7d55604eeacf29f43c0731ef0a69db935cafbf21
Moch. Nafi'ul Irsad Al A <nafiulirsad@gmail.com>
2023-06-12 00:13:10 +0700

    Upgrade build.gradle plugin, delete unnecessary line code AndroidManifest.xml and change SplashActivity.kt to Splash.kt for avoid warning, and fine-tuning layout, fonts and layout constraints.

--- app/src/main/java/com/fitdev/findindonesiatourism/ui/activity/splash/Splash.kt
+++ app/src/main/java/com/fitdev/findindonesiatourism/ui/activity/splash/Splash.kt
@@ -1,21 +1,17 @@
 package com.fitdev.findindonesiatourism.ui.activity.splash
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import com.fitdev.myapplication.R
+import androidx.appcompat.app.AppCompatActivity
 import com.fitdev.myapplication.databinding.ActivitySplashBinding
 
-class SplashActivity : AppCompatActivity() {
-
+class Splash : AppCompatActivity() {
     private lateinit var binding : ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
